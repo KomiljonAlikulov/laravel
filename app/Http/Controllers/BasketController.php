de735cfcca270d6ceecab27238c08f9d2a5c3f6e
Aleksey <alekseydanilin75@gmail.com>
2020-05-05 14:00:29 +0300

    закончили 8 видео

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Order;
+use App\Product;
 use Illuminate\Http\Request;
 
 class BasketController extends Controller
@@ -16,9 +17,22 @@ class BasketController extends Controller
         return view('basket', compact('order'));
     }
 
-    public function basketConfirm()
+    public function basketConfirm(Request $request)
     {
+        $orderId = session('orderId');
+        if (is_null($orderId)) {
+            return redirect()->route('index');
+        }
+        $order = Order::find($orderId);
+        $success = $order->saveOrder($request->name, $request->phone);
 
+        if ($success) {
+            session()->flash('success', 'Ваш заказ принят в обработку!');
+        } else {
+            session()->flash('warning', 'Случилась ошибка');
+        }
+
+        return redirect()->route('index');
     }
 
     public function basketPlace()
@@ -49,6 +63,10 @@ class BasketController extends Controller
             $order->products()->attach($productId);
         }
 
+        $product = Product::find($productId);
+
+        session()->flash('success', 'Добавлен товар ' . $product->name);
+
         return redirect()->route('basket');
     }
 
@@ -69,6 +87,10 @@ class BasketController extends Controller
                 $pivotRow->update();
             }
         }
+
+        $product = Product::find($productId);
+
+        session()->flash('warning', 'Удален товар  ' . $product->name);
 
         return redirect()->route('basket');
     }
